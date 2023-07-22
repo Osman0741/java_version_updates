8f0e51d70c421a43d5ffea8aa34db11c39afd433
LENOVO <osmnozdmrosh@gmail.com>
2023-07-22 14:19:03 +0300

    record

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,10 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+
+    //All Arg Constructor
+    //Getter
+    //To String
+    //Equals
+    //Hash Code
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,13 @@
+package com.cydeo.java14;
+
+public class UserTest {
+
+    public static void main(String[] args) {
+
+        User user = new User ("Osman","TR");
+        System.out.println(user.name());
+        System.out.println(user.toString());
+    }
+
+
+}
